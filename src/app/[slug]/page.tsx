8eb658743f6834577d5b45736eb3d26d91c5c146
Truong Son Dev <truongsonpt.80@gmail.com>
2023-06-12 16:20:42 +0700

    upodate

--- src/app/[slug]/page.tsx
+++ src/app/[slug]/page.tsx
@@ -1,5 +1,20 @@
+import Image from "next/image";
+import { Suspense } from "react";
+
 async function getData(id: string) {
-    const res = await fetch(`https://backoffice.nodemy.vn/api/blogs/${id}`);
+    const res = await fetch(`https://backoffice.nodemy.vn/api/blogs/${id}`, {
+        next: { tags: ["post"] },
+    });
+    // The return value is *not* serialized
+    // You can return Date, Map, Set, etc.
+
+    return res.json();
+}
+
+async function getDataPhoto() {
+    const res = await fetch(
+        `https://jsonplaceholder.typicode.com/photos?_limit=15`
+    );
     // The return value is *not* serialized
     // You can return Date, Map, Set, etc.
 
@@ -9,7 +24,8 @@ async function getData(id: string) {
 // Return a list of `params` to populate the [slug] dynamic segment
 export async function generateStaticParams() {
     const posts = await fetch(
-        "https://backoffice.nodemy.vn/api/blogs?poplate=*"
+        "https://backoffice.nodemy.vn/api/blogs?poplate=*",
+        { next: { revalidate: 0 } }
     );
 
     const data = await posts.json();
@@ -17,8 +33,6 @@ export async function generateStaticParams() {
     const dataBuild = data.data.map((item: any) => ({
         slug: item.attributes.slug,
     }));
-
-    console.log(dataBuild);
 
     return dataBuild;
 }
@@ -31,8 +45,7 @@ interface IPage {
 
 export default async function Page({ params }: IPage) {
     const data = await getData(params.slug);
-
-    console.log(data);
+    const dataPhoto = await getDataPhoto();
 
     return (
         <main>
@@ -41,6 +54,38 @@ export default async function Page({ params }: IPage) {
                     __html: data?.data?.attributes?.content,
                 }}
             ></div>
+            <Suspense fallback={<div>Loading...</div>}>
+                <ul>
+                    {dataPhoto.map((album: any) => (
+                        <li key={album.id}>
+                            {
+                                // eslint-disable-next-line @next/next/no-img-element
+                                <img src={album.url} alt="" />
+                            }
+                        </li>
+                    ))}
+                </ul>
+                {/* <Albums promise={dataPhoto} /> */}
+            </Suspense>
         </main>
+    );
+}
+
+// Albums Component
+async function Albums({ promise }: { promise: Promise<any[]> }) {
+    // Wait for the albums promise to resolve
+    const albums = await promise;
+
+    return (
+        <ul>
+            {albums.map((album: any) => (
+                <li key={album.id}>
+                    {
+                        // eslint-disable-next-line @next/next/no-img-element
+                        <img src={album.url} alt="" />
+                    }
+                </li>
+            ))}
+        </ul>
     );
 }
